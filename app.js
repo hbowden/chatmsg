29e83f51e1b888531cedfd50154af013faa14c32
nah <2trill2spill@gmail.com>
2016-09-09 13:46:58 -0500

    Add base node express server.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/heroes');
+
+// Serve back static files
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+// Handle index file separately
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+})
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});
