3b1600b23982a9e7393302877b03627e92d3b82a
nah <2trill2spill@gmail.com>
2016-09-09 15:06:20 -0500

    Remove mongoose and add deepstream library.
    Deepstream will handle storage so no need for mongoose.

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 var express = require('express');
+var Deepstream = require('deepstream.io');
+var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DBSTRING || 'mongodb://localhost/heroes');
+// var mongoose = require('mongoose');
+// mongoose.connect(process.env.DBSTRING || 'mongodb://localhost/heroes');
 
-// Serve back static files
+/* Explicitly create a HTTP server from our express app. */
+var server = http.createServer(app);
+
+/* Serve back static files */
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './public')));
 
-// Handle index file separately
+/* Handle index file separately */
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
 })
 
+/* Try to get our port from the enviroment variable PORT, if the
+   enviroment variable PORT is not set default to 5000.  */
 app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() {
-    console.log('Listening on port: ', app.get('port'));
-});
+
+/* Setup deepstream.io server. We use deepstream to sync message history
+   across clients for messaging services that don't automatically do this. */
+var deepstream = new Deepstream();
+deepstream.set('httpServer', server );
+deepstream.start();
+
+/* Use server listen instead app listen so we can use deepstream. */
+server.listen(app.get('port'));
